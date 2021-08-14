4e739e0925d1e7bab070897848a2c78769e4f452
ThitinatIM-kiyuu <75411172+ThitinatIM-kiyuu@users.noreply.github.com>
2021-08-15 00:53:03 +0700

    update people list

--- src/views/peopleAndDetail/PeopleDetail.vue
+++ src/views/peopleAndDetail/PeopleDetail.vue
@@ -2,6 +2,7 @@
   <p>Name: {{people.name}} <br> Surname: {{people.Surname}} </p>
   <p>Age : {{people.age}}</p>
   <p>Home Town : {{people.hometown}}</p>
+  <img v-bind:src= "people.img" />
 
 </template>
 
