dd999454d8eb90b2512099cfedf44da569acef18
ThitinatIM-kiyuu <75411172+ThitinatIM-kiyuu@users.noreply.github.com>
2021-08-14 21:40:07 +0700

    create VaccineDetail

---
+++ src/views/peopleAndDetail/VaccineDetail.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Name: {{ Vaccine.name}}</p>
+  <p>Detail: {{ Vaccine.detail}}</p>
+  <p>Country: {{ Vaccine.Country}}</p>
+
+</template>
+
+<script>
+
+export default {
+  props: ['Vaccine'],
+  
+}
+</script>
