24a4a64fd4bec795afcbdaa422e1018a7e81e00a
Mukiisanma1412 <86796241+Mukiisanma1412@users.noreply.github.com>
2021-08-15 19:59:28 +0700

    Add name to form and show it in comment list

--- src/views/peopleAndDetail/DoctorComment.vue
+++ src/views/peopleAndDetail/DoctorComment.vue
@@ -2,13 +2,19 @@
   <p>Doctor's comment on {{people.name}}'s vaccination' </p>
       <form class="review-form">
             <h3>Doctor review</h3>
-      
+            <input id="name" v-model="name">
             <p><label for="comment">Comment:</label>
             <textarea id="comment" v-model="comment"></textarea></p>
       
             <button type="button" class="btn btn-light" @click="submit">Light</button>
         </form>
-        {{ Comments }}
+
+        <!-- {{ Comments}} -->
+
+        <ul>
+          <li v-for="Acomment in Comments" v-bind:key="Acomment.name">{{Acomment.name}} : {{Acomment.comment}} </li>
+        </ul>
+
 </template>
 
 <script>
@@ -17,12 +23,17 @@ export default {
   props: ['people', 'Comments'],
   data(){
     return{
-     
+      name: '',
+      comment: ''
     }
   },
   methods:{
     submit(){
-      this.$emit('comment', this.comment);
+      let newComment ={
+        name : this.name,
+        comment : this.comment
+      }
+      this.$emit('comment', newComment);
     }
   } 
 }
