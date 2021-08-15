97a77078ce589287439fb9047a6d8bccc943421a
Mukiisanma1412 <86796241+Mukiisanma1412@users.noreply.github.com>
2021-08-15 20:10:48 +0700

    Change UI

--- src/views/peopleAndDetail/DoctorComment.vue
+++ src/views/peopleAndDetail/DoctorComment.vue
@@ -1,40 +1,53 @@
 <template>
-  <p>Doctor's comment on {{people.name}}'s vaccination' </p>
-      <form class="review-form">
-            <h3>Doctor review</h3>
-            <input id="name" v-model="name">
-            <p><label for="comment">Comment:</label>
-            <textarea id="comment" v-model="comment"></textarea></p>
-      
-            <button type="button" class="btn btn-light" @click="submit">Light</button>
-        </form>
+  <p>Doctor's comment on {{ people.name }}'s vaccination'</p>
 
-        <!-- {{ Comments}} -->
+  <form>
+    <div class="mb-3">
+      <label for="name" class="form-label">Name</label>
+      <input type="text" class="form-control" id="name" v-model="name" />
+    </div>
+    <div class="mb-3">
+      <label for="comment" class="form-label">Example textarea</label>
+      <textarea
+        class="form-control"
+        id="comment"
+        rows="3"
+        v-model="comment"
+      ></textarea>
+    </div>
+    <button type="button" class="btn btn-primary" @click="submit">
+      Submit
+    </button>
+  </form>
 
-        <ul>
-          <li v-for="Acomment in Comments" v-bind:key="Acomment.name">{{Acomment.name}} : {{Acomment.comment}} </li>
-        </ul>
+  <!-- {{ Comments}} -->
 
+  <ul>
+    <li v-for="Acomment in Comments" v-bind:key="Acomment.name">
+      {{ Acomment.name }} : {{ Acomment.comment }}
+    </li>
+  </ul>
 </template>
 
 <script>
-
 export default {
-  props: ['people', 'Comments'],
-  data(){
-    return{
-      name: '',
-      comment: ''
-    }
+  props: ["people", "Comments"],
+  data() {
+    return {
+      name: "",
+      comment: "",
+    };
   },
-  methods:{
-    submit(){
-      let newComment ={
-        name : this.name,
-        comment : this.comment
-      }
-      this.$emit('comment', newComment);
-    }
-  } 
-}
+  methods: {
+    submit() {
+      let newComment = {
+        name: this.name,
+        comment: this.comment,
+      };
+      this.$emit("comment", newComment);
+      this.name = ""
+      this.comment = ""
+    },
+  },
+};
 </script>
