7ee71eec3b298a004942c9e7c5bbefb07e2cf5a4
ThitinatIM-kiyuu <75411172+ThitinatIM-kiyuu@users.noreply.github.com>
2021-08-15 18:38:47 +0700

    Doctor comment page

--- src/views/peopleAndDetail/DoctorComment.vue
+++ src/views/peopleAndDetail/DoctorComment.vue
@@ -1,5 +1,15 @@
 <template>
   <p>Doctor's comment on {{people.name}}'s vaccination' </p>
+  <form class="review-form" @submit.prevent="onSubmit">
+            <h3>Doctor review</h3>
+            <p><label for="name">Name:</label>
+            <input id="name" v-model="name"></p>
+            <p><label for="review">Review:</label>
+            <textarea id="review" v-model="review"></textarea></p>
+            
+            <input class="button" type="submit" value="Submit">
+        </form>
+        
 </template>
 
 <script>
